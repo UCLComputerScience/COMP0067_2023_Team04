fadb492ad209a960f47f258411c27f52852028a3
UCABZ <119633725+UCABZ@users.noreply.github.com>
2023-03-06 19:27:01 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './navigation/AdminTabs';
+import UserTabs from './navigation/UserTabs';
 
 const App = () =>{
   return(
