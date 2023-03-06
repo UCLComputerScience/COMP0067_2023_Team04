5c7ba5982f965403723f40b5e9ece06e18eb05bf
UCABZ <119633725+UCABZ@users.noreply.github.com>
2023-03-06 19:01:27 +0000

    original screen created

---
+++ src/screens/UserDevicesScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserDevicesScreen = () => {
+  return (
+    <View>
+      <Text>UserDevicesScreen</Text>
+    </View>
+  )
+}
+
+export default UserDevicesScreen
