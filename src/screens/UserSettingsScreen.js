75c9618b83d2f0de99f5462abbdfcd91f40c982b
JunmingHuJim <114617873+JunmingHuJim@users.noreply.github.com>
2023-03-12 20:38:56 +0000

    Add files via upload

--- src/screens/UserSettingsScreen.js
+++ src/screens/UserSettingsScreen.js
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const UserSettingsScreen = () => {
+
+const UserSettingsScreen =props=> {
+  console.log(props);//显示prop属性
+  console.log(props.navigation);//显示navigation属性
   return (
-    <View>
-      <Text>UserSettingsScreen</Text>
-    </View>
-  )
-}
+  <View>
+    <Text style={styles.text}>HomeScreen</Text>
+    <Button 
+    onPress={()=> props.navigation.navigate('LoginTab')}//terminal日志会出现一行（）
+    title = "Go to components demo" 
+    />
+  </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
 
 export default UserSettingsScreen
