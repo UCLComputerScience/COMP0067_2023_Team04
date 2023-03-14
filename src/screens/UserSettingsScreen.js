0376c48295f936fc6abe4fa2463a2228bcbea0a6
UCABZ <119633725+UCABZ@users.noreply.github.com>
2023-03-14 02:01:44 +0000

    1

--- src/screens/UserSettingsScreen.js
+++ src/screens/UserSettingsScreen.js
@@ -1,26 +1,129 @@
-import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 
 
-const UserSettingsScreen =props=> {
-  console.log(props);//显示prop属性
-  console.log(props.navigation);//显示navigation属性
+const UserSettingsScreen = () => {
+  const [isLogoutModalVisible, setIsLogoutModalVisible] = useState(false);
+
+  const handleLogout = () => {
+    Alert.alert(
+      'Are you sure?',
+      'Are you sure you want to logout of the application?',
+      [
+        {
+          text: 'YES',
+          onPress: () => console.log('YES Pressed')
+        },
+        {
+          text: 'NO',
+          onPress: () => console.log('NO Pressed'),
+          style: 'cancel'
+        }
+      ],
+      { cancelable: false }
+    )
+  };
+
+  const handleLogoutConfirm = () => {
+    setIsLogoutModalVisible(false);
+  };
+
+  const handleLogoutCancel = () => {
+    setIsLogoutModalVisible(false);
+  };
+
   return (
-  <View>
-    <Text style={styles.text}>HomeScreen</Text>
-    <Button 
-    onPress={()=> props.navigation.navigate('LoginTab')}//terminal日志会出现一行（）
-    title = "Go to components demo" 
-    />
-  </View>
+    <View style={styles.container}>
+
+
+      
+      <View style={styles.sectionContainer}>
+
+        <TouchableOpacity style={styles.section} activeOpacity={0.8}>
+          <Text style={styles.sectionTitle}>Personal Details</Text>
+          <Text style={styles.sectionSubTitle}>&gt;</Text>
+        </TouchableOpacity>
+
+        <View style={styles.generalContainer}>
+          <Text style={styles.generalTitle}>General</Text>
+        </View>
+
+        <TouchableOpacity style={styles.section} activeOpacity={0.8}>
+          <Text style={styles.sectionTitle}>About</Text>
+          <Text style={styles.sectionSubTitle}>Version 1.0.0 &gt;</Text>
+        </TouchableOpacity>
+
+      </View>
+
+      <TouchableOpacity style={styles.section} activeOpacity={0.8} onPress={handleLogout}>
+        <Text style={styles.sectionTitle}>Log Out</Text>
+      </TouchableOpacity>
+      
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
+  
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
   },
+
+
+
+  sectionContainer: {
+    marginBottom: 30,
+  },
+
+
+  section: {
+    backgroundColor: '#fff',
+    paddingVertical: 16,
+    paddingHorizontal: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+
+
+  sectionTitle: {
+    fontFamily: 'System',
+    fontSize: 14,
+    color: '#333',
+  },
+
+
+  sectionSubTitle: {
+    fontSize: 14,
+    color: '#ccc',
+  },
+
+
+  generalContainer: {
+    height: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+
+  generalTitle: {
+    paddingLeft: 12,
+    fontSize: 12,
+    color: '#333',
+  },
+
+
+  generalSubTitle: {
+    fontSize: 20,
+    color: '#ccc',
+    marginLeft: 4,
+  },
+ 
 });
-
-
-export default UserSettingsScreen
+export default UserSettingsScreen;
