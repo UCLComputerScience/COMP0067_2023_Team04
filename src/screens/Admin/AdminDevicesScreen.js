529b99cddbe98aa3c508aa68c02dec14888881c4
KennyDAI2000 <1823963114@qq.com>
2023-03-19 22:03:24 +0000

    Search function available

--- src/screens/Admin/AdminDevicesScreen.js
+++ src/screens/Admin/AdminDevicesScreen.js
@@ -1,17 +1,57 @@
-import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TextInput, StyleSheet, FlatList } from 'react-native'
+import React, {useState} from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const AllDevices = () => {
+
+  const devices = [
+    {
+    name: 'Lenovo Legion Y9000P 2022 RTX 3070ti',
+    Loaned: '10',
+    Availabe: '5',
+    },
+    {
+      name: 'Lenovo Legion Y9000P 2022 RTX 3070',
+      Loaned: '6',
+      Availabe: '9',
+    },    
+  ];
+
+  const [input, setInput] = useState('');
+
   return(
     <ScrollView style={ styles.container }  contentInset={{ top: 0, bottom: 90 }}>
       <View style={styles.searchbar}>
         <Icon name="search" size={20} color="#000" />
-        <TextInput 
+        <TextInput
+          value= {input} 
+          onChangeText = {(text) => setInput(text)}
           style = {{fontSize: 15}}
           placeholder="Search"
         />
+      </View>
+      <View style={styles.list} >
+        <FlatList data={devices} renderItem={({item}) => {
+            if(input === ""){
+              return (
+                <View style={styles.line}>
+                  <Text style={[styles.devices, {flex:3, textAlign: 'left'}]}>{item.name}</Text>
+                  <Text style={[styles.devices, {flex:1, textAlign: 'center'}]}>{item.Loaned}</Text>
+                  <Text style={[styles.devices, {flex:1, textAlign: 'center'}]}>{item.Availabe}</Text>
+                </View>
+              )
+            }
+            if(item.name.toLowerCase().includes(input.toLowerCase())){
+              return (
+                <View style={styles.line}>
+                  <Text style={[styles.devices, {flex:3, textAlign: 'left'}]}>{item.name}</Text>
+                  <Text style={[styles.devices, {flex:1, textAlign: 'center'}]}>{item.Loaned}</Text>
+                  <Text style={[styles.devices, {flex:1, textAlign: 'center'}]}>{item.Availabe}</Text>
+                </View>
+              )
+            }
+        }}/>
       </View>
     </ScrollView>
   );
@@ -37,6 +77,19 @@ const styles =StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  list: {
+    marginTop:10,
+  },
+  line:{
+    marginVertical:15,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+  },
+  devices:{
+    fontSize: 16,
+    fontWeight: 'bold',
+    
   }
 })
 
