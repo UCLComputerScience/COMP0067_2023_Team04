ad0ae38000f34f8ab139b18b1d1d9c6dc6c7bb15
KennyDAI2000 <1823963114@qq.com>
2023-03-20 16:47:54 +0000

    Update GeneralDeviceAdmin.js

--- src/screens/Admin/GeneralDeviceAdmin.js
+++ src/screens/Admin/GeneralDeviceAdmin.js
@@ -1,6 +1,7 @@
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
 import { useRoute } from '@react-navigation/native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const GeneralDeviceAdmin = () => {
 
@@ -20,38 +21,72 @@ const GeneralDeviceAdmin = () => {
   }]
   const summaryDetailsUnpacked = JSON.parse(device[0].summaryDetails);
 
+  const [expanded, setExpanded] = useState(false);
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <ScrollView style={styles.container} contentInset={{ top: 0, bottom: 90 }}>
       <View style={styles.titleView}>
-        <Text style = {styles.title}>{deviceName}</Text>
+        <Text style={styles.title}>{deviceName}</Text>
       </View>
-      <View style={{marginHorizontal:50}}>
-      {Object.entries(summaryDetailsUnpacked).map(([key, value]) => (
-        <View key={key} style={{ flexDirection: 'row' }}>
-          <Text style={{ fontWeight: 'bold', flex: 1 }}>{key}:</Text>
-          <Text style={{ flex: 2 }}>{value}</Text>
-        </View>
-      ))}
+
+      
+      
+      <View style={styles.tabBar}>
+        <TouchableOpacity style={styles.tabButton} onPress={toggleExpand}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Text style={[styles.tabButtonText, { flex: 1 }]}>Summary Details</Text>
+            <Ionicons style={{ marginLeft: 'auto' }} name={expanded ? 'chevron-up' : 'chevron-down'} size={16} color={'#AC145A'} />
+          </View>
+        </TouchableOpacity>
+        {expanded && (
+          <View style={{ marginHorizontal: 50 }}>
+            {Object.entries(summaryDetailsUnpacked).map(([key, value]) => (
+              <View key={key} style={{ flexDirection: 'row', paddingVertical:2 }}>
+                <Text style={{ fontWeight: '500', flex: 1 }}>{key}:</Text>
+                <Text style={{ fontWeight:'300', flex: 2 }}>{value}</Text>
+              </View>
+            ))}
+          </View>
+        )}
       </View>
     </ScrollView>
-  )
+  );
+  
 }
 
 const styles =StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingHorizontal: 20,
   },
   titleView:{
     alignItems:'center',
-    paddingHorizontal:80,
+    paddingHorizontal:50,
     paddingVertical: 10
   },
   title:{
-    fontSize:21,
+    fontSize:22,
     fontWeight: 'bold',
     textAlign: 'center',
-  }
+  },
+  tabBar: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+    marginTop: -5,
+  },
+  tabButton: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  tabButtonText: {
+    fontSize: 16,
+    fontWeight: '700'
+  },
 })
 
 export default GeneralDeviceAdmin
