231c5304ff14c9cbb7a9f4c2c9cd9d528aadaf48
UCABZ <119633725+UCABZ@users.noreply.github.com>
2023-03-23 15:14:45 +0000

    User device page

---
+++ src/screens/User/GeneralDeviceUser.js
@@ -0,0 +1,305 @@
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+import { useRoute, useNavigation } from '@react-navigation/native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+const GeneralDeviceUser = () => {
+
+  const navigation = useNavigation();
+  const route = useRoute();
+  const deviceName = route.params;
+
+  const device = [{
+    standardLoanDuration: 14,
+    extensionAllowance: 1,
+    summaryDetails: '{"CPU": "Intel Core i9-12900H Octo-core 20 threads", \
+                      "GPU": "RTX 3070ti 8G 150W", \
+                      "Memory": "DDR5 16GB 4800Hz Dual", \
+                      "SSD": "SAMSUNG PM9A1 512GB", \
+                      "Screen": "2.5K (2560*1600) 16:10 165Hz", \
+                      "Power": "300W", \
+                      "WIFI": "AX211"}',
+  }]
+
+  const devices = [
+    { id: 20220901001, state: 'Loaned'      },
+    { id: 20220901002, state: 'Available'   },
+    { id: 20220901003, state: 'Maintained'  },
+    { id: 20220901004, state: 'Loaned'      },
+    { id: 20220901005, state: 'Loaned'      },
+    { id: 20220901006, state: 'Available'   },
+    { id: 20220901007, state: 'Loaned'      },
+    { id: 20220901008, state: 'Available'   },
+    { id: 20220901009, state: 'Available'   },
+    { id: 20220901010, state: 'Loaned'      },
+    { id: 20220901011, state: 'Available'   },
+    { id: 20220901012, state: 'Available'   },
+  ];
+  const summaryDetailsUnpacked = JSON.parse(device[0].summaryDetails);
+
+  const history =[
+    {
+      deviceID: 20220901001,
+      userID: 'ucabcda',
+      date: '2023-03-21'
+    },
+    {
+      deviceID: 20220901001,
+      userID: 'ucabccb',
+      date: '2023-03-20'
+    },
+    {
+      deviceID: 20220901002,
+      userID: 'ucabcdb',
+      date: '2023-03-20'
+    },
+    {
+      deviceID: 20220901003,
+      userID: 'ucabcdc',
+      date: '2023-03-19'
+    }
+  ]
+
+  const [loanRuleExpanded, setLoanRuleExpanded] = useState(false);
+  const [summaryDetailsExpanded, setSummaryDetailsExpanded] = useState(false);
+  const [devicesIDExpanded, setDevicesIDExpanded] = useState(false);
+  const [loanHistoryExpanded, setLoanHistoryExpanded] = useState(false);
+
+  const toggleLoanRule = () => {
+    setLoanRuleExpanded(!loanRuleExpanded);
+  };
+  const toggleSummaryDetails = () => {
+    setSummaryDetailsExpanded(!summaryDetailsExpanded);
+  };
+  const toggleDevicesID = () => {
+    setDevicesIDExpanded(!devicesIDExpanded);
+  };
+  const toggleLoanHistory = () => {
+    setLoanHistoryExpanded(!loanHistoryExpanded);
+  };
+
+  return (
+    <ScrollView style={styles.container} contentInset={{ bottom: 100 }}>
+
+      <View style={styles.titleView}>
+        <Text style={styles.title}>{deviceName}</Text>
+      </View>
+
+      <View style={styles.tabBar}>
+        <TouchableOpacity style={styles.tabButton} onPress={toggleLoanRule}>
+          <View style={styles.detailLayout}>
+            <Text style={[styles.tabButtonText, { flex: 1 }]}>Loan Rules</Text>
+            <Ionicons style={{ marginLeft: 'auto' }} name={loanRuleExpanded ? 'chevron-up' : 'chevron-down'} size={16} color={'#AC145A'} />
+          </View>
+        </TouchableOpacity>
+        {loanRuleExpanded && (
+          <View style={styles.detailRow}>            
+              <View style={styles.detailRowLayout}>
+                <Text style={{ fontWeight: '500', flex: 3 }}>Standard Loan Duration:</Text>
+                <Text style={{ fontWeight:'300', flex: 1 }}>{device[0].standardLoanDuration} Days</Text>
+              </View>
+              <View style={styles.detailRowLayout}>
+                <Text style={{ fontWeight: '500', flex: 3 }}>Extension Allowance:</Text>
+                <Text style={{ fontWeight:'300', flex: 1 }}>
+                  {parseInt(device[0].extensionAllowance) > 1 ? device[0].extensionAllowance + ' Times' : device[0].extensionAllowance + ' Time'}
+                </Text>
+              </View>
+          </View>
+        )}
+      </View>
+      
+      <View style={styles.tabBar}>
+        <TouchableOpacity style={styles.tabButton} onPress={toggleSummaryDetails}>
+          <View style={styles.detailLayout}>
+            <Text style={[styles.tabButtonText, { flex: 1 }]}>Summary Details</Text>
+            <Ionicons style={{ marginLeft: 'auto' }} name={summaryDetailsExpanded ? 'chevron-up' : 'chevron-down'} size={16} color={'#AC145A'} />
+          </View>
+        </TouchableOpacity>
+        {summaryDetailsExpanded && (
+          <View style={styles.detailRow}>
+            {Object.entries(summaryDetailsUnpacked).map(([key, value]) => (
+              <View key={key} style={styles.detailRowLayout}>
+                <Text style={{ fontWeight:'500', flex: 1 }}>{key}:</Text>
+                <Text style={{ fontWeight:'300', flex: 2 }}>{value}</Text>
+              </View>
+            ))}
+          </View>
+        )}
+      </View>
+
+
+      <View style={styles.tabBar}>
+        <TouchableOpacity style={styles.tabButton} onPress={toggleDevicesID}>
+          <View style={styles.detailLayout}>
+            <Text style={[styles.tabButtonText, { flex: 1 }]}>Loan options</Text>
+            <Ionicons style={{ marginLeft: 'auto' }} name={devicesIDExpanded ? 'chevron-up' : 'chevron-down'} size={16} color={'#AC145A'} />
+          </View>
+        </TouchableOpacity>
+        {devicesIDExpanded && (
+          <View style={styles.detailRow}>
+              <View style={styles.detailRowLayout}>
+                <Text style={[styles.deviceKey, {flex:2, paddingLeft:10}]}>
+                  Device
+                </Text>
+                <Text style={[styles.deviceKey, {flex:1, paddingLeft:-10, textAlign:'center'}]}>
+                  State
+                </Text>
+              </View>
+              <View style={styles.detailRowLayout}>
+                <View style={{ flex: 2 }}>
+                  {devices.map(device => (
+                    <TouchableOpacity key={device.id} onPress={() => navigation.navigate('Detail', {deviceID: device.id})}>
+                      <Text style={styles.deviceID}>{device.id}</Text>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+                <View style={{ flex: 1 }}>
+                  {devices.map(device => (
+                    <Text key={device.id} style={styles.deviceState}>{device.state}</Text>
+                  ))}
+                </View>
+              </View>
+
+          </View>
+        )}
+      </View>
+
+
+      <View style={styles.tabBar}>
+        <TouchableOpacity style={styles.tabButton} onPress={toggleLoanHistory}>
+          <View style={styles.detailLayout}>
+            <Text style={[styles.tabButtonText, { flex: 1 }]}>Loan History</Text>
+            <Ionicons style={{ marginLeft: 'auto' }} name={loanHistoryExpanded ? 'chevron-up' : 'chevron-down'} size={16} color={'#AC145A'} />
+          </View>
+        </TouchableOpacity>
+        {loanHistoryExpanded && (
+          <View style={styles.detailRow}>
+          <View style={styles.detailRowLayout}>
+            <Text style={[styles.deviceKey, {flex:1, paddingLeft:10}]}>
+              Device ID
+            </Text>
+            <Text style={[styles.deviceKey, {flex:1, textAlign:'center'}]}>
+              User ID
+            </Text>
+            <Text style={[styles.deviceKey, {flex:1, textAlign:'center'}]}>
+              Date
+            </Text>
+          </View>
+          <View style={styles.detailRowLayout}>
+            <Text style={styles.historyDeviceID}>
+              {history.map(history => `${history.deviceID}`).join('\n')}
+            </Text>
+            <Text style={styles.historyUserID}>
+              {history.map(history => `${history.userID}`).join('\n')}
+            </Text>
+            <Text style={[styles.historyState, {flex:1}]}>
+              {history.map(history => `${history.date}`).join('\n')}
+            </Text>
+          </View>
+      </View>
+        )}
+      </View>
+
+
+
+    </ScrollView>
+  );
+  
+}
+
+const styles =StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+  },
+  titleView:{
+    alignItems:'center',
+    paddingHorizontal:50,
+    paddingVertical: 10,
+  },
+  title:{
+    fontSize:22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  tabBar: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  tabButton: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#D6D6D6'
+  },
+  tabButtonText: {
+    fontSize: 16,
+    fontWeight: '700'
+  },
+  detailLayout:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  detailRow:{
+    marginHorizontal: 30,
+    marginTop: 10
+  },
+  detailRowLayout:{
+    flexDirection: 'row',
+    paddingVertical:2,
+  },
+
+  deviceKey:{
+    fontWeight: '500',  
+    lineHeight: 20, 
+    fontSize: 16,
+  },
+  deviceID:{
+    fontWeight: '300', 
+    flex: 2, 
+    lineHeight: 20,
+    textDecorationLine: 'underline'
+  },
+  deviceState:{
+    fontWeight: '300', 
+    flex: 1, 
+    lineHeight: 20, 
+    textAlign:'center'
+  },
+
+  historyDeviceID:{
+    fontWeight: '300', 
+    flex: 1.1, 
+    lineHeight: 20,
+    textAlign:'left'
+  },
+  historyUserID:{
+    fontWeight: '300', 
+    flex: 0.6, 
+    lineHeight: 20,
+    textAlign:'center'
+  },
+  historyState:{
+    fontWeight: '300', 
+    flex: 1, 
+    lineHeight: 20,
+    textAlign:'right'
+  },
+})
+
+const Stack = createStackNavigator();
+
+const GeneralDeviceUserScreen = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+      <Stack.Screen name="General" component={GeneralDeviceUser} />
+      
+    </Stack.Navigator>
+  )
+}
+
+export default GeneralDeviceUserScreen
--- src/screens/User/UserDevicesScreen.js
+++ src/screens/User/UserDevicesScreen.js
@@ -1,11 +1,266 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, FlatList, TouchableOpacity, Alert } from 'react-native'
+import React, {useState} from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import GeneralDeviceUser from './GeneralDeviceUser';
 
+const AllDevices = () => {
+
+  const navigation = useNavigation();
+  const route = useRoute();
+  const initialDevices = [
+    {
+      name: 'Lenovo Legion Y9000P 2022 RTX 3070ti',
+      launchedyear: '2021',
+      availabe: '5',
+    },
+    {
+      name: 'Lenovo Legion Y9000P 2022 RTX 3070',
+      launchedyear: '2022',
+      availabe: '9',
+    },
+    {
+      name: 'Lenovo Legion Y9000P 2022 RTX 3060',
+      launchedyear: '2023',
+      availabe: '8',
+    },
+    {
+      name: 'Dell XPS 13 2022',
+      launchedyear: '2022',
+      availabe: '3',
+    },
+    {
+      name: 'MacBook Pro M1 2021',
+      launchedyear: '2021',
+      availabe: '7',
+    },
+    {
+      name: 'ASUS ROG Zephyrus S GX701',
+      launchedyear: '2022',
+      availabe: '2',
+    },
+    {
+      name: 'Lenovo Legion Y740',
+      launchedyear: '2023',
+      availabe: '4',
+    },
+    {
+      name: 'Acer Predator Helios 300',
+      launchedyear: '2021',
+      availabe: '3',
+    },
+    {
+      name: 'MSI GE76 Raider',
+      launchedyear: '2021',
+      availabe: '8',
+    },
+    {
+      name: 'Razer Blade Pro 17',
+      launchedyear: '2022',
+      availabe: '9',
+    },
+    {
+      name: 'Alienware m15 R4',
+      launchedyear: '2020',
+      availabe: '6',
+    },
+    {
+      name: 'HP Spectre x360',
+      launchedyear: '2018',
+      availabe: '10',
+    },        
+  ];
+
+  const handleicon = () => {
+    Alert.alert(
+      'Contact information',
+      'Email: uclcsdevice@ucl.ac.uk\nPhone: +44 07412356987\nAddress: MP Engineering Building',
+      [
+        {
+          text: 'YES',
+          onPress: () => console.log('YES Pressed'),
+          style: 'cancel'
+        }
+      ],
+      { 
+        cancelable: false,
+        
+      }
+    )
+  };
+
+  const [input, setInput] = useState('');
+  const [devices, setDevices] = useState(initialDevices);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [loanedSortOrder, setLoanedSortOrder] = useState('asc');
+  const [availableSortOrder, setAvailableSortOrder] = useState('asc');
+
+  const sortDevices = (order) => {
+    const sortedDevices = [...devices].sort((a, b) => {
+      if (order === 'asc') {
+        return a.name.localeCompare(b.name);
+      } else {
+        return b.name.localeCompare(a.name);
+      }
+    });
+    setDevices(sortedDevices);
+  };
+
+  const sortDevicesByLoaned = (order) => {
+    const sortedDevices = [...devices].sort((a, b) => {
+      if (order === 'asc') {
+        return a.launchedyear - b.launchedyear;
+      } else {
+        return b.launchedyear - a.launchedyear;
+      }
+    });
+    setDevices(sortedDevices);
+  };
+
+  
+
+  const sortDevicesByAvailable = (order) => {
+    const sortedDevices = [...devices].sort((a, b) => {
+      if (order === 'asc') {
+        return a.availabe - b.availabe;
+      } else {
+        return b.availabe - a.availabe;
+      }
+    });
+    setDevices(sortedDevices);
+  };
+
+  const handleLoanedSort = () => {
+    const newSortOrder = loanedSortOrder === 'asc' ? 'desc' : 'asc';
+    setLoanedSortOrder(newSortOrder);
+    sortDevicesByLoaned(newSortOrder);
+  };
+
+  const handleAvailableSort = () => {
+    const newSortOrder = availableSortOrder === 'asc' ? 'desc' : 'asc';
+    setAvailableSortOrder(newSortOrder);
+    sortDevicesByAvailable(newSortOrder);
+  };
+
+  return(
+    <View style={ styles.container}>
+      <View style={{paddingHorizontal:20}}>
+        <View style={{flexDirection: 'row'}}>
+          <View style={styles.searchbar}>
+            <Icon name="search" size={20} color="#000" />
+            <TextInput
+              value= {input} 
+              onChangeText = {(text) => setInput(text)}
+              style = {{fontSize: 15}}
+              placeholder="Search"
+            />
+          </View>
+          <TouchableOpacity onPress={handleicon}>
+          <Ionicons style = {styles.add} size={25} name="information-circle-outline"/>
+          
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.list} >
+      <View style={{ marginVertical: 5, paddingHorizontal: 30, flexDirection: 'row' }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', flex: 3 }}>
+            <Text style={[styles.header, { textAlign: 'left' }]}>
+              Devices
+            </Text>
+          </View>
+        <TouchableOpacity onPress={handleLoanedSort} style={{ flexDirection: 'row', alignItems: 'center', flex: 1, marginLeft: 20 }}>
+          <Text style={[styles.header, { textAlign: 'center' }]}>Launched year</Text>
+          <Ionicons name={`chevron-${loanedSortOrder === 'asc' ? 'up' : 'down'}-outline`} size={15} color="#AC145A" style={{ marginHorizontal: 0 }} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleAvailableSort} style={{ flexDirection: 'row', alignItems: 'center', flex: 1, marginLeft: 30 }}>
+          <Text style={[styles.header, { textAlign: 'center' }]}>Available</Text>
+          <Ionicons name={`chevron-${availableSortOrder === 'asc' ? 'up' : 'down'}-outline`} size={15} color="#AC145A" style={{ marginHorizontal: 0 }} />
+        </TouchableOpacity>
+      </View>
+        <FlatList data={devices} renderItem={({item} ) => {
+            if(input === ""){
+              return (
+                <TouchableOpacity onPress={() => navigation.navigate('GeneralDeviceUser', { deviceName: item.name })}>
+                  <View style={styles.line}>
+                    <Text style={[styles.devices, {flex:2.4, textAlign: 'left'}]}>{item.name}</Text>
+                    <Text style={[styles.devices, {marginLeft:-20, flex:1, textAlign: 'center'}]}>{item.launchedyear}</Text>
+                    <Text style={[styles.devices, {marginRight:-5, flex:0.9, textAlign: 'center'}]}>{item.availabe}</Text>
+                  </View>
+                </TouchableOpacity>
+              )
+            }
+            if(item.name.toLowerCase().includes(input.toLowerCase())){
+              return (
+                <View style={styles.line}>
+                  <Text style={[styles.devices, {flex:3, textAlign: 'left'}]}>{item.name}</Text>
+                  <Text style={[styles.devices, {flex:1, textAlign: 'center'}]}>{item.launchedyear}</Text>
+                  <Text style={[styles.devices, {MARflex:1, textAlign: 'center'}]}>{item.availabe}</Text>
+                </View>
+              )
+            }
+        }}
+        contentContainerStyle={{ paddingBottom: 170 }} 
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles =StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  searchbar: {
+    marginTop: 10,
+    padding: 10,
+    flexDirection: "row",
+    width: '80%',
+    backgroundColor: '#F2F2F2',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  add:{
+    marginLeft: 20,
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+    flex: 1
+  },
+  search: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  list: {
+    marginTop:10,
+  },
+  line:{
+    marginVertical:15,
+    paddingHorizontal: 30,
+    flexDirection: 'row',
+  },
+  header:{
+    fontSize: 12,
+    fontWeight: 'bold', 
+    color: '#A6AAB2',
+  },
+  devices:{
+    fontSize: 16,
+    fontWeight: 'bold',    
+  }
+})
+
+const Stack = createStackNavigator();
 const UserDevicesScreen = () => {
   return (
-    <View>
-      <Text>UserDevicesScreen</Text>
-    </View>
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+      <Stack.Screen name="AllDevices" component={AllDevices} />
+      <Stack.Screen name="GeneralDeviceUser" component={GeneralDeviceUser} />
+    </Stack.Navigator>
   )
 }
 
