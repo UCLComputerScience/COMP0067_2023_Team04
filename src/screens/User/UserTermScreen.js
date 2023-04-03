ab2dabe7c0900ba68ff5bad9d06721636cdad230
UCABZ <119633725+UCABZ@users.noreply.github.com>
2023-04-03 17:34:58 +0100

    1

--- src/screens/User/UserTermScreen.js
+++ src/screens/User/UserTermScreen.js
@@ -1,13 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const UserTermScreen = () => {
+const UserTermScreen = ({ navigation }) => {
+  const handleAgreePress = () => {
+    console.log('I agree button pressed');
+    navigation.navigate('GeneralDeviceUser2');
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Term of Use</Text>
+      <TouchableOpacity style={styles.button} onPress={handleAgreePress}>
+        <Text style={styles.buttonText}>I agree</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-      </View>
-  )
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  },
+});
 
-export default UserTermScreen
+export default UserTermScreen;
