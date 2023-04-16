061d533858eac8ea7a1096c9e02c861db34926dc
timothytfchan <59618579+timothytfchan@users.noreply.github.com>
2023-04-16 18:11:53 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://20.254.93.210:8080/",
+    baseURL: "https://20.254.93.210:8080/",
     headers: {
         "Content-type": "application/json"
     }
