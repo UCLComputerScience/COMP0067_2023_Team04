3f46303a18c2c2b995a809d7c4a9e31a5627b1af
KennyDAI2000 <1823963114@qq.com>
2023-04-16 11:21:31 +0100

    Logout available

--- src/screens/Admin/AdminSettingsScreen.js
+++ src/screens/Admin/AdminSettingsScreen.js
@@ -1,26 +1,27 @@
-import { View, Text, Button } from 'react-native';
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, Button } from "react-native";
+import React, { useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
+import AuthContext from "../Navigation/AuthContext";
 
 const AdminSettingsScreen = () => {
   const navigation = useNavigation();
+  const { logout } = useContext(AuthContext);
 
   const handleLogout = () => {
     // logout logic here
     // for example, remove the user token or reset the user state
     parentNavigation = navigation.getParent();
-    parentNavigation.goBack('Login'); // navigate back to the login screen after logout
+    parentNavigation.goBack("Login"); // navigate back to the login screen after logout
     const state = navigation.getState();
     console.log(state);
   };
 
   //const parentNavigation = navigation.getParent();
 
-
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>AdminSettingsScreen</Text>
-      <Button title="Logout" onPress={handleLogout} />
+      <Button title="Logout" onPress={logout} />
     </View>
   );
 };
--- src/screens/Navigation/LoginTab.js
+++ src/screens/Navigation/LoginTab.js
@@ -1,119 +1,83 @@
-import React, { useState } from 'react';
-import { Image, View, Text, TextInput, Button, Linking} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import AdminTabScreen from './AdminTabs';
-import UserTabScreen from './UserTabs';
+import React, { useState, useContext } from "react";
+import { View, Text, Button, SafeAreaView } from "react-native";
+import {
+  NavigationContainer,
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
+import AdminTabs from "./AdminTabs";
+import UserTabs from "./UserTabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import AuthContext from "./AuthContext";
+
+const LoginTabScreen = () => {
+  const navigation = useNavigation();
+  const { loginAsAdmin, loginAsUser } = useContext(AuthContext);
+
+  return (
+    <SafeAreaView>
+      <Button title="SSO Login" />
+      <Button title="Admin Login" onPress={loginAsAdmin} />
+      <Button title="User Login" onPress={loginAsUser} />
+    </SafeAreaView>
+  );
+};
 
 const Stack = createStackNavigator();
-const CLIENT_ID = "0830044936260096.0010110257374561";
 
-function generateRandomString(length) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
+const LoginTab = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-function LoginTab() {
-  const [userRole, setUserRole] = useState(null);
+  const loginAsAdmin = () => {
+    setIsAdmin(true);
+    setLoggedIn(true);
+  };
 
-  useEffect(() => {
-    const handleDeepLink = async (event) => {
-      const url = new URL(event.url);
-      const code = url.searchParams.get('code');
-      const state = url.searchParams.get('state');
+  const loginAsUser = () => {
+    setIsAdmin(false);
+    setLoggedIn(true);
+  };
 
-      try {
-        const response = await fetch(`https://your-server.com/oauth/callback?code=${code}&state=${state}`);
-        const data = await response.json();
+  const logout = () => {
+    setLoggedIn(false);
+    setIsAdmin(false);
+  };
 
-        if (data.success) {
-          setUserRole(data.role);
-        } else {
-          // Handle error
-        }
-      } catch (error) {
-        // Handle error
-      }
-    };
-
-    Linking.addEventListener('url', handleDeepLink);
-
-    return () => {
-      Linking.removeEventListener('url', handleDeepLink);
-    };
-  }, []);
-
-  const handleLogin = () => {
-    const url = `https://uclapi.com/oauth/authorise?client_id=` + CLIENT_ID + `&state=` + generateRandomString(16);
-    Linking.openURL(url);
+  const authContextValue = {
+    loggedIn,
+    isAdmin,
+    loginAsAdmin,
+    loginAsUser,
+    logout,
   };
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName = 'Login'>
-        {userRole === 'admin' ? (
-          <Stack.Screen
-            name="AdminTab"
-            component={AdminTabScreen}
-            options={{ title: 'Admin Tab Screen' }}
-          />
-        ) : userRole === 'user' ? (
-          <Stack.Screen
-            name="UserTab"
-            component={UserTabScreen}
-            options={{ title: 'User Tab Screen' }}
-          />
+    <AuthContext.Provider value={authContextValue}>
+      <NavigationContainer>
+        {loggedIn ? (
+          isAdmin ? (
+            <AdminTabs />
+          ) : (
+            <UserTabs />
+          )
         ) : (
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{ title: 'Login', headerShown: true }}
-          />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-
-  function LoginScreen({ navigation }) {
-    const [username, setUsername] = useState('');
-
-    // For frontend development only
-    function handleAdminLogin() {
-      const role = 'admin';
-      setUserRole(role);
-    }
-
-    function handleUserLogin() {
-      const role = 'user';
-      setUserRole(role);
-    }
-    //
-
-    return (
-      <View style={{ flex: 1, padding: 20, backgroundColor: '#fff' }}>
-        <Image 
-            source={require('../../components/icons/UCL.png')}
-            resizeMode = 'contain'
-            style={{
-                width: 100,
-                height: 50,
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+              headerLeft: null,
+              gestureEnabled: false,
+              headerBackVisible: false,
             }}
-        />
-        <Text style={{ fontWeight: 'bold', fontSize: 24, marginBottom: 10, textAlign: 'left' }}>Sign in</Text>
-        <View style={{ flexDirection: 'row', justifyContent: 'center', marginRight: 20 }}>
-          <Button title="SSO Login" onPress={handleLogin} />
-        </View >
-        <View style={{ flex:0.6 ,flexDirection: 'row', justifyContent: 'center', alignItems:'center' }}>
-          <Button title="Admin Login" onPress={handleAdminLogin} />
-          <Button title="User Login" onPress={handleUserLogin} />
-        </View>
-      </View>
-    );
-  }
-}
+          >
+            <Stack.Screen name="LoginTabScreen" component={LoginTabScreen} />
+            <Stack.Screen name="AdminTabs" component={AdminTabs} />
+            <Stack.Screen name="UserTabs" component={UserTabs} />
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+};
 
 export default LoginTab;
