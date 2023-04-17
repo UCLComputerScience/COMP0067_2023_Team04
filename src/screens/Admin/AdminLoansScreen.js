e3706c1cc4523ec13fe6c61b896958d9ac653518
KennyDAI2000 <1823963114@qq.com>
2023-04-17 22:12:08 +0100

    fetch from db, finalyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- src/screens/Admin/AdminLoansScreen.js
+++ src/screens/Admin/AdminLoansScreen.js
@@ -10,60 +10,34 @@ import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import DetailDeviceAdmin from "./DetailDeviceAdmin";
+import axios from "axios";
 
+const API_BASE_URL = "http://20.254.93.210:8080/posts/";
 const AdminLoans = () => {
   // All loan information, where id is the device ID
-  const loanTable = [
-    {
-      name: "Lenovo Legion Y9000P 2022 RTX 3070ti",
-      user: "ucabj38",
-      state: "Available",
-      returnDate: "2023-04-21",
-      id: 20220901008,
-    },
-    {
-      name: "Lenovo Legion Y9000P 2022 RTX 3080ti",
-      user: "ucabj38",
-      state: "Loan",
-      returnDate: "2023-03-14",
-      id: 20220901001,
-    },
-    {
-      name: "Lenovo Legion Y9000P 2022 RTX 3060ti",
-      user: "ucabj38",
-      state: "Loan",
-      returnDate: "2023-03-15",
-      id: 20220901002,
-    },
-    {
-      name: "Lenovo Legion Y9000P 2022 RTX 3050ti",
-      user: "ucabj38",
-      state: "Loan",
-      returnDate: "2023-03-16",
-      id: 20220901003,
-    },
-    {
-      name: "Lenovo Legion Y9000P 2022 RTX 2070ti",
-      user: "ucabj38",
-      state: "Loan",
-      returnDate: "2023-03-17",
-      id: 20220901004,
-    },
-    {
-      name: "Lenovo Legion Y9000P 2022 RTX 4070ti",
-      user: "ucabj38",
-      state: "Loan",
-      returnDate: "2023-03-18",
-      id: 20220901005,
-    },
-    {
-      name: "Lenovo Legion Y9000P 2022 RTX 4060ti",
-      user: "ucabj38",
-      state: "Loan",
-      returnDate: "2023-03-19",
-      id: 20220901006,
-    },
-  ];
+  const [loanTable, setLoanTable] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/loans`);
+      console.log(response.data);
+      setLoanTable(
+        response.data.map((loan) => ({
+          id: loan.deviceId,
+          name: `Device ${loan.deviceId}`,
+          returnDate: loan.dueDate.substring(0, 10),
+          user: loan.userId,
+          state: new Date(loan.dueDate) >= new Date() ? "Loan" : "Returned",
+        }))
+      );
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const navigation = useNavigation();
 
@@ -89,7 +63,9 @@ const AdminLoans = () => {
     filterData(tab);
   };
   useEffect(() => {
-    filterData(currentTab);
+    fetchData().then(() => {
+      filterData(currentTab);
+    });
   }, []);
 
   return (
@@ -122,80 +98,82 @@ const AdminLoans = () => {
           </Text>
         </TouchableOpacity>
       </View>
-      {currentTab === "Overdue" ? (
-        <FlatList
-          data={filteredData}
-          renderItem={({ item }) => {
-            return (
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate("Detail", { deviceID: item.id });
-                }}
-              >
-                <View style={styles.line}>
-                  <View style={{ flex: 2, alignItems: "flex-start" }}>
-                    <Text style={[styles.devices, { textAlign: "left" }]}>
-                      {item.name}
-                    </Text>
-                    <Text style={[styles.user, { textAlign: "center" }]}>
-                      User ID: {item.user}
+      <View style={{ paddingBottom: 115 }}>
+        {currentTab === "Overdue" ? (
+          <FlatList
+            data={filteredData}
+            renderItem={({ item }) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("Detail", { deviceID: item.id });
+                  }}
+                >
+                  <View style={styles.line}>
+                    <View style={{ flex: 2, alignItems: "flex-start" }}>
+                      <Text style={[styles.devices, { textAlign: "left" }]}>
+                        {item.name}
+                      </Text>
+                      <Text style={[styles.user, { textAlign: "center" }]}>
+                        User ID: {item.user}
+                      </Text>
+                    </View>
+                    <Text
+                      style={[styles.date, { flex: 1.4, textAlign: "center" }]}
+                    >
+                      {item.returnDate}
                     </Text>
                   </View>
-                  <Text
-                    style={[styles.date, { flex: 1.4, textAlign: "center" }]}
-                  >
-                    {item.returnDate}
-                  </Text>
-                </View>
-                <View style={styles.separator} />
-              </TouchableOpacity>
-            );
-          }}
-        />
-      ) : currentTab === "All history" ? (
-        <FlatList
-          data={filteredData}
-          renderItem={({ item }) => {
-            const returnDate = new Date(item.returnDate);
-            const today = new Date();
-            today.setHours(0, 0, 0, 0);
+                  <View style={styles.separator} />
+                </TouchableOpacity>
+              );
+            }}
+          />
+        ) : currentTab === "All history" ? (
+          <FlatList
+            data={filteredData}
+            renderItem={({ item }) => {
+              const returnDate = new Date(item.returnDate);
+              const today = new Date();
+              today.setHours(0, 0, 0, 0);
 
-            const isOverdue =
-              item.state === "Loan" && returnDate.getTime() < today.getTime();
-            return (
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate("Detail", { deviceID: item.id });
-                }}
-              >
-                <View style={styles.line}>
-                  <View style={{ flex: 2, alignItems: "flex-start" }}>
-                    <Text style={[styles.devices, { textAlign: "left" }]}>
-                      {item.name}
-                    </Text>
-                    <Text style={[styles.user, { textAlign: "center" }]}>
-                      User ID: {item.user}
+              const isOverdue =
+                item.state === "Loan" && returnDate.getTime() < today.getTime();
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("Detail", { deviceID: item.id });
+                  }}
+                >
+                  <View style={styles.line}>
+                    <View style={{ flex: 2, alignItems: "flex-start" }}>
+                      <Text style={[styles.devices, { textAlign: "left" }]}>
+                        {item.name}
+                      </Text>
+                      <Text style={[styles.user, { textAlign: "center" }]}>
+                        User ID: {item.user}
+                      </Text>
+                    </View>
+                    <Text
+                      style={[
+                        styles.date,
+                        {
+                          flex: 1.4,
+                          textAlign: "center",
+                          color: isOverdue ? "red" : "black",
+                        },
+                      ]}
+                    >
+                      {item.returnDate}
                     </Text>
                   </View>
-                  <Text
-                    style={[
-                      styles.date,
-                      {
-                        flex: 1.4,
-                        textAlign: "center",
-                        color: isOverdue ? "red" : "black",
-                      },
-                    ]}
-                  >
-                    {item.returnDate}
-                  </Text>
-                </View>
-                <View style={styles.separator} />
-              </TouchableOpacity>
-            );
-          }}
-        />
-      ) : null}
+                  <View style={styles.separator} />
+                </TouchableOpacity>
+              );
+            }}
+          />
+        ) : null}
+      </View>
     </View>
   );
 };
