2c33d24463573bcf93209ea160ef868d6d96c7f9
timothytfchan <59618579+timothytfchan@users.noreply.github.com>
2023-04-17 02:39:00 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,20 +3,29 @@ const fetch = require('node-fetch');
 const BASE_URL = "http://20.254.93.210:8080/";
 
 const api = {
-  get: async (path) => {
-    try {
-      const response = await fetch(`${BASE_URL}${path}`, {
-        method: "GET",
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-      return await response.json();
-    } catch (error) {
-      console.error("Fetch error:", error);
-      throw error;
-    }
-  },
-};
+    get: async (path) => {
+      try {
+        const response = await fetch(`${BASE_URL}${path}`, {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+  
+        // Check if the response is OK (status code 200) and the content type is JSON
+        if (response.ok && response.headers.get('content-type').includes('application/json')) {
+          return await response.json();
+        } else {
+          // If the response is not OK or the content type is not JSON, log the error and the response text
+          console.error("Fetch error: Invalid response or content type", response.status, await response.text());
+          throw new Error("Invalid response or content type");
+        }
+      } catch (error) {
+        console.error("Fetch error:", error);
+        throw error;
+      }
+    },
+  };
+  
 
 export default api;
