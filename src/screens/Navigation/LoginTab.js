968c11031d0be519b907d6d0c3101d82f3ca49d7
JunmingHuJim <114617873+JunmingHuJim@users.noreply.github.com>
2023-04-17 11:31:57 +0100

    Update LoginTab.js

--- src/screens/Navigation/LoginTab.js
+++ src/screens/Navigation/LoginTab.js
@@ -1,44 +1,18 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Image, View, Text, Button, Linking, AsyncStorage } from 'react-native';
+import React, { useState } from 'react';
+import { Image, View, Text, TextInput, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AdminTabScreen from './AdminTabs';
 import UserTabScreen from './UserTabs';
-import jwtDecode from 'jwt-decode';
-import api from '../../config';
-import AuthContext from './AuthContext';
 
 const Stack = createStackNavigator();
 
 function LoginTab() {
-  const { userRole, setUserRole } = useContext(AuthContext);
-
-  useEffect(() => {
-    const handleDeepLink = async (event) => {
-      const url = new URL(event.url);
-      const jwtToken = url.searchParams.get('token');
-
-      try {
-        const decoded = jwtDecode(jwtToken);
-        const { role, apiToken } = decoded;
-
-        setUserRole(role);
-        await AsyncStorage.setItem('accessToken', apiToken);
-      } catch (error) {
-        // Handle error
-      }
-    };
-
-    Linking.addEventListener('url', handleDeepLink);
-
-    return () => {
-      Linking.removeEventListener('url', handleDeepLink);
-    };
-  }, []);
+  const [userRole, setUserRole] = useState(null);
 
   return (
-
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName = 'Login'>
         {userRole === 'admin' ? (
           <Stack.Screen
             name="AdminTab"
@@ -59,41 +33,50 @@ function LoginTab() {
           />
         )}
       </Stack.Navigator>
+    </NavigationContainer>
   );
-}
 
-function LoginScreen() {
-  const handleLogin = () => {
-    api.get('oauth').then((response) => {
-      console.log('API response:', response);
-      if (response.data && response.data.url) {
-        Linking.openURL(response.data.url);
-      } else {
-        // Handle error
-        console.error('Unexpected API response format');
-      }
-    }).catch((error) => {
-      // Handle error
-      console.error('API request error:', error);
-    });
-  };
+  function LoginScreen({ navigation }) {
+    const [username, setUsername] = useState('');
 
-  return (
-    <View style={{ flex: 1, padding: 20, backgroundColor: '#fff' }}>
-      <Image
-        source={require('../../components/icons/UCL.png')}
-        resizeMode="contain"
-        style={{
-          width: 100,
-          height: 50,
-        }}
-      />
-      <Text style={{ fontWeight: 'bold', fontSize: 24, marginBottom: 10, textAlign: 'left' }}>Sign in</Text>
-      <View style={{ flexDirection: 'row', justifyContent: 'center', marginRight: 20 }}>
-        <Button title="SSO Login" onPress={handleLogin} />
+    function handleLogin() {
+      // SSO login logic
+      const role = 'user'; // Assume successful login
+      setUserRole(role);
+    }
+
+    // For frontend development only
+    function handleAdminLogin() {
+      const role = 'admin';
+      setUserRole(role);
+    }
+
+    function handleUserLogin() {
+      const role = 'user';
+      setUserRole(role);
+    }
+
+    return (
+      <View style={{ flex: 1, padding: 20, backgroundColor: '#fff' }}>
+        <Image 
+            source={require('../../components/icons/UCL.png')}
+            resizeMode = 'contain'
+            style={{
+                width: 100,
+                height: 50,
+            }}
+        />
+        <Text style={{ fontWeight: 'bold', fontSize: 24, marginBottom: 10, textAlign: 'left' }}>Sign in</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginRight: 20 }}>
+          <Button title="SSO Login" onPress={handleLogin} />
+        </View >
+        <View style={{ flex:0.6 ,flexDirection: 'row', justifyContent: 'center', alignItems:'center' }}>
+          <Button title="Admin Login" onPress={handleAdminLogin} />
+          <Button title="User Login" onPress={handleUserLogin} />
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 export default LoginTab;
