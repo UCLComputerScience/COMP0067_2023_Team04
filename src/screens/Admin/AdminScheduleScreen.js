15bd8b36f04ed14bc58d44fa094e515f90496c39
JunmingHuJim <114617873+JunmingHuJim@users.noreply.github.com>
2023-04-17 14:08:46 +0100

    Update AdminScheduleScreen.js

--- src/screens/Admin/AdminScheduleScreen.js
+++ src/screens/Admin/AdminScheduleScreen.js
@@ -31,7 +31,8 @@ const CollapsibleList = () => {
   };
   /*please notice. We only load this week data*/
   //device with state 'reserved' or ('loan' && 'duedate < friday'
-  const loanTable = [
+  /*please notice. We only load this week data*/
+  /*const loanTable = [
     {
       name: "Lenovo Legion Y9000P 2022 RTX 3070ti",
       user: "ucabj38",
@@ -74,7 +75,7 @@ const CollapsibleList = () => {
       state: "Loan",
       startDate: "2023-03-19",
     },
-  ];
+  ];*/
 
   const now = new Date();
 
