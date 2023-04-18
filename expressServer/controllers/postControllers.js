35085309655b2bb935f396dd3c0a3989ecbdc48b
timothytfchan <59618579+timothytfchan@users.noreply.github.com>
2023-04-18 14:59:40 +0100

    added routes + controllers

--- expressServer/controllers/postControllers.js
+++ expressServer/controllers/postControllers.js
@@ -10,6 +10,7 @@ exports.getAllDevices = async (req, res, next) => {
   }
 };
 
+//for DetailDeviceAdmin.js, specific device by ID and details and latest loan
 exports.getDeviceById = async (req, res, next) => {
   try {
     const device = await deviceModel.getDeviceById(req.params.id);
@@ -23,6 +24,27 @@ exports.getDeviceById = async (req, res, next) => {
   }
 };
 
+//for AdminDevicesScreen.js (gets deviceName, num_loaned, num_available, category)
+exports.getDevicesByNameAvailability = async (req, res, next) => {
+  try {
+    const devices = await deviceModel.getDevicesByNameAvailability();
+    res.status(200).json(devices);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching devices by name and availability', error });
+  }
+};
+
+//for AdminScheduleScreen.js (loads devices reserved & overdue/due this week)
+exports.getSchedule = async (req, res, next) => {
+  try {
+    const schedule = await loanModel.getSchedule();
+    res.status(200).json(schedule);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching schedule', error });
+  }
+};
+
+// Misc
 exports.getAllLoans = async (req, res, next) => {
   try {
     const loans = await loanModel.getAllLoans();
@@ -32,6 +54,7 @@ exports.getAllLoans = async (req, res, next) => {
   }
 };
 
+// Misc
 exports.getLoanById = async (req, res, next) => {
   try {
     const loan = await loanModel.getLoanById(req.params.id);
@@ -45,24 +68,7 @@ exports.getLoanById = async (req, res, next) => {
   }
 };
 
-exports.getLoansByUserId = async (req, res) => {
-  try {
-    const loans = await loanModel.getLoansByUserId(req.params.userId);
-    res.status(200).json(loans);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.getLoansByDeviceId = async (req, res) => {
-  try {
-    const loans = await loanModel.getLoansByDeviceId(req.params.deviceId);
-    res.status(200).json(loans);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
+// Misc
 exports.getDeviceSummary = async (req, res) => {
   try {
     const deviceSummary = await deviceModel.getDeviceSummary();
@@ -80,221 +86,104 @@ exports.getDeviceSummary = async (req, res) => {
   }
 };
 
-exports.getSchedule = async (req, res) => {
+// for GeneralDeviceAdmin.js (given name of device, find its details)
+exports.getDetailsByDeviceName = async (req, res, next) => {
   try {
-    const schedule = await loanModel.getSchedule();
-    res.status(200).json(schedule);
+    const deviceName = req.query.name;
+    const device = await deviceModel.getDetailsByDeviceName(deviceName);
+    res.status(200).json(device);
   } catch (error) {
-    res.status(500).json({error: 'An error occurred while fetching the schedule.' });
+    res.status(500).json({ message: 'Error fetching device details', error });
   }
 };
 
-exports.createLoan = async (req, res) => {
+// for GeneralDeviceAdmin.js (given name of device, get all device IDs and associated states)
+exports.getIdByName = async (req, res, next) => {
   try {
-    const { userId, startDate, dueDate, deviceId, exten, scheduledCollectionDate, scheduledReturnDate } = req.body;
-    const newLoan = await loanModel.createLoan(userId, startDate, dueDate, deviceId, exten, scheduledCollectionDate, scheduledReturnDate);
-    res.status(201).json(newLoan);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.updateLoan = async (req, res) => {
-  try {
-    const updated = await loanModel.updateLoan(req.params.loanId, req.body);
-    if (updated) {
-      res.status(200).json({ message: 'Loan updated successfully' });
-    } else {
-      res.status(404).json({ message: 'Loan not found' });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.deleteLoan = async (req, res) => {
-  try {
-    const deleted = await loanModel.deleteLoan(req.params.loanId);
-    if (deleted) {
-      res.status(200).json({ message: 'Loan deleted successfully' });
-    } else {
-      res.status(404).json({ message: 'Loan not found' });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.getLoansWithDeviceNamesForCurrentWeek = async (req, res) => {
-  try {
-    const loans = await loanModel.getLoansWithDeviceNamesForCurrentWeek();
-    res.status(200).json(loans);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.getLoanHistory = async (req, res) => {
-  try {
-    const history = await loanModel.getLoanHistory();
-    res.status(200).json(history);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.extendLoanDueDate = async (req, res) => {
-  try {
-    const { loanId, newDueDate } = req.body;
-    const extended = await loanModel.extendLoanDueDate(loanId, newDueDate);
-    if (extended) {
-      res.status(200).json({ message: 'Loan due date extended successfully' });
-    } else {
-      res.status(404).json({ message: 'Loan not found' });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.createReservation = async (req, res) => {
-  try {
-  const { userId, deviceId, scheduledCollectionDate } = req.body;
-  const reservation = await loanModel.createReservation(userId, deviceId, scheduledCollectionDate);
-  res.status(201).json(reservation);
-  } catch (err) {
-  res.status(500).json({ error: err.message });
-  }
-  };
-
-exports.getLoansByStatus = async (req, res) => {
-  try {
-  const { userId, status } = req.body;
-  const loans = await loanModel.getLoansByStatus(userId, status);
-  res.status(200).json(loans);
-  } catch (err) {
-  res.status(500).json({ error: err.message });
-  }
-  };
-
-exports.updateDeviceState = async (req, res) => {
-  try {
-    const deviceId = parseInt(req.params.deviceId);
-    const newState = req.body.newState;
-
-    const success = await deviceModel.updateDeviceState(deviceId, newState);
-    if (success) {
-      res.status(200).json({ message: 'Device state updated' });
-    } else {
-      res.status(400).json({ message: 'Failed to update device state' });
-    }
-  } catch (error) {
-    res.status(500).json({ message: 'Error updating device state', error });
-  }
-};
-// TODO
-exports.getAllDevicesUser = async (req, res) => {
-  try {
-    const devices = await deviceModel.getAllDevicesUser();
+    const deviceName = req.query.name;
+    const devices = await deviceModel.getIdByName(deviceName);
     res.status(200).json(devices);
   } catch (error) {
-    res.status(500).json({ message: 'Failed to get all devices for user', error });
+    res.status(500).json({ message: 'Error fetching device IDs', error });
   }
 };
 
-// More controllers
-/*
-exports.getLoansByUserId = async (req, res, next) => {
-    try {
-    const loans = await loanModel.getLoansByUserId(req.params.userId);
-    res.status(200).json(loans);
-    } catch (error) {
-    res.status(500).json({ message: 'Error fetching loans by user ID', error });
+// for GeneralDeviceAdmin.js (given name of a device, find all associated loan history)
+exports.getLoanHistoryByName = async (req, res, next) => {
+  try {
+    const deviceName = req.params.name;
+    const loanHistory = await deviceModel.getLoanHistoryByName(deviceName);
+    res.status(200).json(loanHistory);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching loan history', error });
+  }
+};
+
+// for GeneralDeviceUser.js (returns details of first available device by its name)
+exports.getDevicebyName = async (req, res, next) => {
+  try {
+    const deviceName = req.params.name;
+    const device = await deviceModel.getDevicebyName(deviceName);
+    res.status(200).json(device);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching device by name', error });
+  }
+};
+
+// for UserDevicesScreen.js (gets deviceName, launchedYear, num_available, category)
+exports.getDevicesByNameAvailabilityUser = async (req, res, next) => {
+  try {
+    const devices = await deviceModel.getDevicesByNameAvailabilityUser();
+    res.status(200).json(devices);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching devices for user by name and availability', error });
+  }
+};
+
+// for PastDeviceScreen.js (returns all loan history for a specific user)
+exports.getLoanHistoryByUser = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const loanHistory = await deviceModel.getLoanHistoryByUser(userId);
+    res.status(200).json(loanHistory);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching loan history by user', error });
+  }
+};
+
+// for UserAppointmentScreen.js (selects loans where the state is reserved and userID is userID)
+exports.getReservedByUser = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const reservedDevices = await deviceModel.getReservedByUser(userId);
+    res.status(200).json(reservedDevices);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching reserved devices by user', error });
+  }
+};
+
+// for UserLoansScreen.js (returns all current loans for a specific user)
+exports.getCurrentLoans = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const currentLoans = await deviceModel.getCurrentLoans(userId);
+    res.status(200).json(currentLoans);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching current loans by user', error });
+  }
+};
+
+// for GeneralDeviceExtendScreen.js (extend specific device by its ID)
+exports.extendLoanById = async (req, res, next) => {
+  try {
+    const deviceId = req.params.id;
+    const updatedLoan = await deviceModel.extendLoanById(deviceId);
+    if (updatedLoan) {
+      res.status(200).json({ message: 'Loan extended successfully', updatedLoan });
+    } else {
+      res.status(404).json({ message: 'Loan not found for the given device ID' });
     }
-    };
-
-exports.createLoan = async (req, res, next) => {
-try {
-const { userId, startDate, dueDate, deviceId, exten, scheduledCollectionDate, scheduledReturnDate } = req.body;
-const newLoan = await loanModel.createLoan(userId, startDate, dueDate, deviceId, exten, scheduledCollectionDate, scheduledReturnDate);
-res.status(201).json(newLoan);
-} catch (error) {
-res.status(500).json({ message: 'Error creating loan', error });
-}
+  } catch (error) {
+    res.status(500).json({ message: 'Error extending loan', error });
+  }
 };
-
-exports.updateLoan = async (req, res, next) => {
-try {
-const updated = await loanModel.updateLoan(req.params.id, req.body);
-if (updated) {
-res.status(200).json({ message: 'Loan updated successfully' });
-} else {
-res.status(404).json({ message: 'Loan not found' });
-}
-} catch (error) {
-res.status(500).json({ message: 'Error updating loan', error });
-}
-};
-
-exports.deleteLoan = async (req, res, next) => {
-try {
-const deleted = await loanModel.deleteLoan(req.params.id);
-if (deleted) {
-res.status(200).json({ message: 'Loan deleted successfully' });
-} else {
-res.status(404).json({ message: 'Loan not found' });
-}
-} catch (error) {
-res.status(500).json({ message: 'Error deleting loan', error });
-}
-};
-
-exports.createLoan = async (req, res, next) => {
-try {
-const { userId, startDate, dueDate, deviceId, exten, scheduledCollectionDate, scheduledReturnDate } = req.body;
-const newLoan = await loanModel.createLoan(userId, startDate, dueDate, deviceId, exten, scheduledCollectionDate, scheduledReturnDate);
-res.status(201).json(newLoan);
-} catch (error) {
-res.status(500).json({ message: 'Error creating loan', error });
-}
-};
-
-exports.updateLoan = async (req, res, next) => {
-try {
-const updated = await loanModel.updateLoan(req.params.id, req.body);
-if (updated) {
-res.status(200).json({ message: 'Loan updated successfully' });
-} else {
-res.status(404).json({ message: 'Loan not found' });
-}
-} catch (error) {
-res.status(500).json({ message: 'Error updating loan', error });
-}
-};
-
-exports.deleteLoan = async (req, res, next) => {
-try {
-const deleted = await loanModel.deleteLoan(req.params.id);
-if (deleted) {
-res.status(200).json({ message: 'Loan deleted successfully' });
-} else {
-res.status(404).json({ message: 'Loan not found' });
-}
-} catch (error) {
-res.status(500).json({ message: 'Error deleting loan', error });
-}
-};
-
-
-exports.getAllPosts = async (req, res, next) => {
-    res.send("Get all posts route");
-};
-
-exports.createNewPost = async (req, res, next) => {
-    res.send("Create New Post Route");
-};
-
-exports.getPostById = async (req, res, next) => {
-    res.send("Get Post By ID Route");
-};
-*/
--- expressServer/routes/postRoutes.js
+++ expressServer/routes/postRoutes.js
@@ -3,43 +3,38 @@ const postControllers = require('../controllers/postControllers');
 const router = express.Router();
 
 // @route GET && POST - /posts/
-router.route('/devices').get(postControllers.getAllDevices);
-router.route('/devices/:id').get(postControllers.getDeviceById);
+// Admin methods
+router.route('/devices').get(postControllers.getAllDevices); // getting details of all devices with unique IDs
+router.route('/devices/:id').get(postControllers.getDeviceById); //for DetailDeviceAdmin.js, specific device by ID and details and latest loan
+router.route('/devices/nameAvailability').get(post.Controllers.getDevicesByNameAvailability); //for AdminDevicesScreen.js (gets deviceName, num_loaned, num_available, category)
+
+router.get('/schedule', postControllers.getSchedule); //for AdminScheduleScreen.js (loads devices reserved & overdue/due this week)
+
+router.get('/details', postControllers.getDetailsByDeviceName); // for GeneralDeviceAdmin.js (given name of device, find its details)
+router.get('/idByName', postControllers.getIdByName); // for GeneralDeviceAdmin.js (given name of device, get all device IDs and associated states)
+router.get('/loansHistoryByName/:name', postControllers.getLoanHistoryByName); // for GeneralDeviceAdmin.js (given name of a device, find all associated loan history)
+
+// router.post('/devices/insertNewDevice'); // for AdminScanScreen.js (INSERTS id, name, type, state, last_updated or date entered)
 
 
+// Miscellaneous methods
 router.route('/loans').get(postControllers.getAllLoans);
 router.route('/loans/:id').get(postControllers.getLoanById);
-
 router.get('/summary', postControllers.getDeviceSummary);
-router.get('/schedule', postControllers.getSchedule);
 
-router.get('/loans/user/:userId', postControllers.getLoansByUserId); //checked
-router.get('/loans/devices/:deviceId', postControllers.getLoansByDeviceId); //checked
-router.post('/loans', postControllers.createLoan); //TODO
-router.put('/loans/:loanId', postControllers.updateLoan); //TODO
-router.delete('/loans/:loanId', postControllers.deleteLoan); //checked
-router.get('/schedule', postControllers.getSchedule); //NEED SUITABLE DATA TO TEST
-router.get('/currentWeekLoans', postControllers.getLoansWithDeviceNamesForCurrentWeek); //NEED SUITABLE DATA TO TEST
-router.get('/loanHistory', postControllers.getLoanHistory); //checked
-router.put('/extendLoanDueDate', postControllers.extendLoanDueDate); //TODO
-router.post('/createReservation', postControllers.createReservation); //TODO
-router.get('/loansByStatus', postControllers.getLoansByStatus); //TODO
 
-router.put('/devices/:deviceId/state', postControllers.updateDeviceState); //TOTEST
-router.get('/user/devices', postControllers.getAllDevicesUser); //TOTEST
+// User methods
+router.get('/devices/deviceByName/:name', postControllers.getDevicebyName); // for GeneralDeviceUser.js (returns details of first available device by its name)
+
+router.get('/devices/nameAvailabilityUser', postControllers.getDevicesByNameAvailabilityUser); // for UserDevicesScreen.js (gets deviceName, launchedYear, num_available, category)
+
+router.get('/loans/loansUser/:userId', postControllers.getLoanHistoryByUser); // for PastDeviceScreen.js (returns all loan history for a specific user)
+router.get('/loans/reservedUser/:userId', postControllers.getReservedByUser); // for UserAppointmentScreen.js (selects loans where the the state is reserved and userID is userID)
+router.get('/loans/loansCurrent/:userId', postControllers.getCurrentLoans); // for UserLoansScreen.js (returns all current loans for a specific user)
+router.post('loans/extend/:id'); // for GeneralDeviceExtendScreen.js (extend specific device by its ID)
+
+
+// N/A for UserSettingsScreen.js
+// N/A for UserTermScreen.js
 
 module.exports = router;
-
-/*
-router
-    .route("/")
-    .get(postControllers.getAllPosts)
-    .post(postControllers.createNewPost);
-
-router.route("/:id").get(postControllers.getPostById);
-router.route('/loans/userId/:userId').get(postControllers.getLoansByUserId);
-router.route('/loans').post(postControllers.createLoan);
-router.route('/loans/:id').put(postControllers.updateLoan);
-router.route('/loans/:id').delete(postControllers.deleteLoan);
-
-*/
