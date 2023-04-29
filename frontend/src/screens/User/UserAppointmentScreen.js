d78eac8c636cbc9047817d2da1d0ec5f1b1505f6
KennyDAI2000 <1823963114@qq.com>
2023-04-29 17:45:44 +0100

    Revert "Update UserAppointmentScreen.js"
    This reverts commit fc0793e3a9d36207a2d3d2f5ae512a7eeeab2d69.

--- frontend/src/screens/User/UserAppointmentScreen.js
+++ frontend/src/screens/User/UserAppointmentScreen.js
@@ -6,195 +6,75 @@ import {
   FlatList,
   TouchableOpacity,
   Alert,
-  ScrollView,
 } from "react-native";
-import React, { useState , useEffect } from "react";
+import React, { useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import Icon from "react-native-vector-icons/Ionicons";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import GeneralDeviceUser from "./GeneralDeviceUser";
+import GeneralDeviceUserScreen from "./GeneralDeviceUser";
 import * as Linking from "expo-linking";
-import axios from "axios";
 
-//This screen needs to read the model number, shelf date, number of available rentals, and name of all available rental devices
-//Note! The number of devices available in the database is 0 should not appear!
+// This screen needs to read the reservation status of the user, it needs the name, status and due date of the device that the user has reserved, the status is only two cases, loan or has been returned
 
-const AllDevices = () => {
+const UserAppointmentScreen = () => {
   const navigation = useNavigation();
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [initialDevices, setInitialDevices] = useState([]);
-
   const route = useRoute();
+
   const currentScreenPath = route.name;
   const currentScreenUrl = Linking.createURL(currentScreenPath);
 
   console.log("Current Screen URL:", currentScreenUrl);
-  const [device, setDevice] = useState(null);
-  const API_BASE_URL = "http://0067team4app.azurewebsites.net/posts";
+  const initialDevices = [
+    {
+      name: "Lenovo Legion Y9000P 2022 RTX 3070ti",
+      status: "Reserved",
+      date: "2022-10-25",
+    },
+    {
+      name: "Lenovo Legion Y9000P 2022 RTX 3070",
+      status: "Reserved",
+      date: "2022-11-25",
+    },
+    {
+      name: "Lenovo Legion Y9000P 2022 RTX 3060",
+      status: "Reserved",
+      date: "2023-01-25",
+    },
+    /*{
+      name: "Dell XPS 13 2022",
+      status: "Return",
+      data: "2023-02-25",
+    },
+    {
+      name: "MacBook Pro M1 2021",
+      status: "Return",
+      data: "2023-03-25",
+    },*/
+  ];
 
-  const [listData, setListData] = useState([]);
+  const [devices, setDevices] = useState(initialDevices);
 
-  const getListData = async () => {
-    const userId = 1;
-    try {
-      const response = await axios.get(
-        `${API_BASE_URL}/devices/nameAvailabilityUser`
-      );
-      console.log("Received data from API:", response.data);
-      setInitialDevices(response.data);
-      setDevices(response.data);
-        
-      if (response.data) {
-        setListData(response.data);
-      }
-    } catch (error) {
-      console.log("error = ", error);
-    }
-  };
-
-  useEffect(() => {
-    getListData();
-  }, []);
-  
-
-  const handleicon = () => {
+  const handleCanel = () => {
     Alert.alert(
-      "Contact information",
-      "Email: uclcsdevice@ucl.ac.uk\nPhone: +44 07412356987\nAddress: MP Engineering Building",
+      "Cancel Reservation",
+      "Are you sure you want to cancel this reservation?",
       [
         {
-          text: "YES",
-          onPress: () => console.log("YES Pressed"),
+          text: "No",
           style: "cancel",
         },
+        {
+          text: "Yes",
+          onPress: () => {
+            Alert.alert("Success", "The reservation is successfully cancelled");
+          },
+        },
       ],
-      {
-        cancelable: false,
-      }
+      { cancelable: false }
     );
-  };
-
-  const [input, setInput] = useState("");
-  const [devices, setDevices] = useState(initialDevices);
-  const [sortOrder, setSortOrder] = useState("asc");
-  const [loanedSortOrder, setLoanedSortOrder] = useState("asc");
-  const [availableSortOrder, setAvailableSortOrder] = useState("asc");
-
-  const sortDevices = (order) => {
-    const sortedDevices = [...devices].sort((a, b) => {
-      if (order === "asc") {
-        return a.name.localeCompare(b.name);
-      } else {
-        return b.name.localeCompare(a.name);
-      }
-    });
-    setDevices(sortedDevices);
-  };
-
-  const sortDevicesByLoaned = (order) => {
-    const sortedDevices = [...devices].sort((a, b) => {
-      if (order === "asc") {
-        return a.launchedyear - b.launchedyear;
-      } else {
-        return b.launchedyear - a.launchedyear;
-      }
-    });
-    setDevices(sortedDevices);
-  };
-
-  const sortDevicesByAvailable = (order) => {
-    const sortedDevices = [...devices].sort((a, b) => {
-      if (order === "asc") {
-        return a.available - b.available;
-      } else {
-        return b.available - a.available;
-      }
-    });
-    setDevices(sortedDevices);
-  };
-
-  const handleLoanedSort = () => {
-    const newSortOrder = loanedSortOrder === "asc" ? "desc" : "asc";
-    setLoanedSortOrder(newSortOrder);
-    sortDevicesByLoaned(newSortOrder);
-  };
-
-  const handleAvailableSort = () => {
-    const newSortOrder = availableSortOrder === "asc" ? "desc" : "asc";
-    setAvailableSortOrder(newSortOrder);
-    sortDevicesByAvailable(newSortOrder);
-  };
-
-  const filterDevicesByCategory = (category) => {
-    if (category === "All") {
-      setDevices(initialDevices);
-    } else {
-      const filteredDevices = initialDevices.filter(
-        (device) => device.category === category
-      );
-      setDevices(filteredDevices);
-    }
-  };
-
-  const handleCategoryTabPress = (category) => {
-    setSelectedCategory(category);
-    filterDevicesByCategory(category);
   };
 
   return (
     <View style={styles.container}>
-      <View style={{ paddingHorizontal: 20 }}>
-        <View style={{ flexDirection: "row" }}>
-          <View style={styles.searchbar}>
-            <Icon name="search" size={20} color="#000" />
-            <TextInput
-              value={input}
-              onChangeText={(text) => setInput(text)}
-              style={{ fontSize: 15 }}
-              placeholder="Search"
-            />
-          </View>
-          <TouchableOpacity onPress={handleicon}>
-            <Ionicons
-              style={styles.add}
-              size={25}
-              name="information-circle-outline"
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      <View style={styles.categoryTabs}>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          {["All", "Laptop", "MacBook", "Android", "iPhone", "CPU", "GPU"].map(
-            (category, index) => (
-              <TouchableOpacity
-                key={category}
-                onPress={() => handleCategoryTabPress(category)}
-                style={[
-                  styles.categoryTab,
-                  selectedCategory === category
-                    ? styles.selectedCategoryTab
-                    : null,
-                ]}
-              >
-                <Text
-                  style={[
-                    styles.categoryTabText,
-                    selectedCategory === category
-                      ? styles.selectedCategoryTabText
-                      : null,
-                  ]}
-                >
-                  {category}
-                </Text>
-              </TouchableOpacity>
-            )
-          )}
-        </ScrollView>
-      </View>
-
       <View style={styles.list}>
         <View
           style={{
@@ -203,32 +83,12 @@ const AllDevices = () => {
             flexDirection: "row",
           }}
         >
-          <View style={{ flexDirection: "row", alignItems: "center", flex: 3 }}>
-            <Text style={[styles.header, { textAlign: "left" }]}>Devices</Text>
-          </View>
-          <TouchableOpacity
-            onPress={handleLoanedSort}
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              flex: 1,
-              marginLeft: 20,
-            }}
-          >
+          <View style={{ flexDirection: "row", alignItems: "center", flex: 2 }}>
             <Text style={[styles.header, { textAlign: "center" }]}>
-              Launch year
+              Devices
             </Text>
-            <Ionicons
-              name={`chevron-${
-                loanedSortOrder === "asc" ? "up" : "down"
-              }-outline`}
-              size={15}
-              color="#AC145A"
-              style={{ marginHorizontal: 0 }}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={handleAvailableSort}
+          </View>
+          <View
             style={{
               flexDirection: "row",
               alignItems: "center",
@@ -236,63 +96,33 @@ const AllDevices = () => {
               marginLeft: 30,
             }}
           >
-            <Text style={[styles.header, { textAlign: "center" }]}>
-              Available
-            </Text>
-            <Ionicons
-              name={`chevron-${
-                availableSortOrder === "asc" ? "up" : "down"
-              }-outline`}
-              size={15}
-              color="#AC145A"
-              style={{ marginHorizontal: 0 }}
-            />
-          </TouchableOpacity>
+            <Text style={[styles.header, { textAlign: "right" }]}>status</Text>
+          </View>
         </View>
         <FlatList
           data={devices}
           renderItem={({ item }) => {
-            if (
-              input === "" ||
-              item.name.toLowerCase().includes(input.toLowerCase())
-            ) {
-              return (
-                <TouchableOpacity
-                  onPress={() =>
-                    navigation.navigate("General Details", {
-                      deviceName: item.name,
-                    })
-                  }
-                >
-                  <View style={styles.line}>
-                    <Text
-                      style={[styles.devices, { flex: 2.4, textAlign: "left" }]}
-                    >
-                      {item.name}
-                    </Text>
-                    <Text
-                      style={[
-                        styles.devices,
-                        { marginLeft: -20, flex: 1, textAlign: "center" },
-                      ]}
-                    >
-                      {item.launchedyear}
-                    </Text>
-                    <Text
-                      style={[
-                        styles.devices,
-                        { marginRight: -5, flex: 0.9, textAlign: "center" },
-                      ]}
-                    >
-                      {item.available}
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-              );
-            }
+            return (
+              <TouchableOpacity onPress={handleCanel}>
+                <View style={styles.line}>
+                  <Text
+                    style={[styles.devices, { flex: 2, textAlign: "left" }]}
+                  >
+                    {item.name}
+                  </Text>
+                  <Text
+                    style={[
+                      styles.devices,
+                      { marginLeft: 10, flex: 1, textAlign: "center" },
+                    ]}
+                  >
+                    {item.status}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            );
           }}
           contentContainerStyle={{ paddingBottom: 170 }}
-          ItemSeparatorComponent={() => <View style={styles.separator} />}
         />
       </View>
     </View>
@@ -333,6 +163,7 @@ const styles = StyleSheet.create({
     marginVertical: 15,
     paddingHorizontal: 30,
     flexDirection: "row",
+    alignItems: "center",
   },
   header: {
     fontSize: 12,
@@ -343,58 +174,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
   },
-  categoryTabs: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginTop: 10,
-  },
-  categoryTab: {
-    paddingVertical: 5,
-    paddingHorizontal: 15,
-  },
-  selectedCategoryTab: {},
-  categoryTabText: {
-    fontSize: 14,
-    fontWeight: "600",
-    color: "#A6AAB2",
-  },
-  selectedCategoryTabText: {
-    color: "#000",
-  },
-  line: {
-    marginVertical: 15,
-    paddingHorizontal: 30,
-    flexDirection: "row",
-    height: 40,
-    alignItems: "center",
-  },
-  separator: {
-    height: 1,
-    width: "90%",
-    backgroundColor: "#EEEEEF",
-    alignSelf: "center",
-  },
 });
 
-const Stack = createStackNavigator();
-const UserDevicesScreen = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="userDevices"
-        component={AllDevices}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="General Details"
-        component={GeneralDeviceUser}
-        options={{
-          headerShown: false,
-          headerTitle: "Details",
-        }}
-      />
-    </Stack.Navigator>
-  );
-};
-
-export default UserDevicesScreen;
+export default UserAppointmentScreen;
