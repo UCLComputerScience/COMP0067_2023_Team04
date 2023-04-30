b301003bde51f2e17f856fd11a18b1f0ce06fff2
timothytfchan <59618579+timothytfchan@users.noreply.github.com>
2023-04-30 16:54:03 +0100

    create txt files and update routes

--- expressServer/routes/postRoutes.js
+++ expressServer/routes/postRoutes.js
@@ -20,8 +20,36 @@ router.get("/loansHistoryByName/:name", verifyToken, postControllers.getLoanHist
 router.get("/loans", verifyToken, postControllers.getAllLoans);
 router.get("/loans/:id", verifyToken, postControllers.getLoanById);
 router.get("/summary", verifyToken, postControllers.getDeviceSummary);
-// gives the username to frontend
-// shows the number of renewals remaining
+
+// weekly events (on MySQL)
+// change reserved -> available if not collected
+// send email to those users who have past the expiry date
+
+// for Admin
+// posts
+// only this week's schedule for reserved devices
+// settings edit timeslot and userTerms, contactinfo
+// include one more line into device, any issues etc.
+// additional states of Maintainance and Scrapped
+// last user ID (low priority)
+// next user ID (who reserved it) (low priority)
+// insert new device data (high priority)
+// statistics (current): total devices, available, overdue (high priority)
+// statistics (yearly): expenditure, loans, on time, overude, most and least popular
+// update device state
+// add device
+// loan, return
+// return new primary keys generated (LAST_INSERT_ID())
+
+// for User
+// fetch personal details of admin: contact information
+// launch year, availability of devices
+// lock device page availability?
+// reserve -> add a row in the loans
+// cancel reservation based on device
+// see if extension possible and return number of extensions left; shows the number of renewals remaining for a specific loan
+// extend
+// fetch user terms
 
 // User methods
 router.get("/devices/nameAvailabilityUser", verifyToken, postControllers.getDevicesByNameAvailabilityUser); // for UserDevicesScreen.js (gets deviceName, launchedYear, num_available, category)
