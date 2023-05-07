cbcdb5e7b9dd474964b7acfe0cc3d01dff6f5ccf
timothytfchan <59618579+timothytfchan@users.noreply.github.com>
2023-05-07 08:16:00 +0100

    Update device_loan_sys_tables.sql

--- device_loan_sys_tables.sql
+++ device_loan_sys_tables.sql
@@ -1,32 +1,23 @@
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
---
--- 主机： 127.0.0.1:3306
--- 生成日期： 2023-02-21 17:36:02
--- 服务器版本： 5.7.36
--- PHP 版本： 7.4.26
 
+-- settings
+-- SET GLOBAL event_scheduler = ON; -- SET in Azure
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
--- SET GLOBAL event_scheduler = ON; -- SET in Azure
+SET time_zone = "+01:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- 数据库： `dl`
---
-
--- --------------------------------------------------------
-
---
--- 表的结构 `device`
---
-
+-- device table
+-- deviceIds are UUIDs
+-- ruleExt refers to how many times a renewal can be made
+-- issues provides extra space to note down any device issues
+-- state can be Available, Reserved, Loaned, Maintenance, and Scrapped
 DROP TABLE IF EXISTS `device`;
 CREATE TABLE IF NOT EXISTS `device` (
   `deviceId` CHAR(36) NOT NULL,
@@ -44,13 +35,6 @@ CREATE TABLE IF NOT EXISTS `device` (
   PRIMARY KEY (`deviceId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- 转存表中的数据 `device`
--- deviceIds are UUIDs
--- ruleExt refers to how many times a renewal can be made
--- issues provides extra space to note down any device issues
--- state can be Available, Reserved, Loaned, Maintenance, and Scrapped
-
 INSERT INTO `device` (`deviceId`, `registerDate`, `state`, `details`, `category`, `name`, `ruleExt`, `ruleDur`, `storage`, `launchYr`, `cost`) VALUES
 ('fb73ee46-e7ac-11ed-93d2-6045bdd1583d', CURRENT_DATE(),'Loaned', '{"CPU": "Intel Core i9-12900H Octo-core 20 threads", "GPU": "RTX 3070ti 8G 150W", "Memory": "DDR5 16GB 4800Hz Dual", "SSD": "SAMSUNG PM9A1 512GB", "Screen": "2.5K (2560*1600) 16:10 165Hz", "Power": "300W", "WIFI": "AX211"}', 'Laptop', 'Lenovo Legion Y9000P 2022 RTX 3070ti', 1, 14, 'A1', 2022, 1000.00),
 ('dab4de3d-e7ac-11ed-93d2-6045bdd1583d', CURRENT_DATE(),'Loaned', '{"CPU": "Intel Core i7-12700H Octo-core 16 threads", "GPU": "RTX 3070 8G 140W", "Memory": "DDR5 16GB 4800Hz Dual", "SSD": "SAMSUNG PM9A1 512GB", "Screen": "2.5K (2560*1600) 16:10 165Hz", "Power": "300W", "WIFI": "AX211"}', 'Laptop', 'Lenovo Legion Y9000P 2022 RTX 3070', 1, 7, 'A2', 2022, 900.00),
@@ -65,14 +49,14 @@ INSERT INTO `device` (`deviceId`, `registerDate`, `state`, `details`, `category`
 ('c3ce447d-e7ac-11ed-93d2-6045bdd1583d', CURRENT_DATE(),'Scrapped', '{"CPU": "Intel Core i9-12900H Octo-core 20 threads", "GPU": "RTX 3070ti 8G 150W", "Memory": "DDR5 16GB 4800Hz Dual", "SSD": "SAMSUNG PM9A1 512GB", "Screen": "2.5K (2560*1600) 16:10 165Hz", "Power": "300W", "WIFI": "AX211"}', 'Laptop', 'Lenovo Legion Y9000P 2022 RTX 3070ti', 1, 14, 'A1', 2022, 1000.00),
 ('cdf8c212-e7ac-11ed-93d2-6045bdd1583d', CURRENT_DATE(),'Maintenance', '{"CPU": "M1 Pro 10-Core", "Memory": "16GB Unified", "SSD": "512GB", "Screen": "14-inch Retina Display", "Ports": "3 x Thunderbolt 4"}', 'Laptop', 'Apple MacBook Pro 2022 14-inch', 1, 14, 'A4', 2022, 1500.00),
 ('dff5d4ba-e7ac-11ed-93d2-6045bdd1583d', CURRENT_DATE(),'Available', '{"CPU": "Intel Core i7-1185G7", "Memory": "16GB LPDDR4x", "SSD": "1TB", "Screen": "14-inch FHD", "Ports": "2 x Thunderbolt 4"}', 'Laptop', 'Lenovo ThinkPad X1 Carbon Gen 9', 1, 14, 'A10', 2021, 1600.00),
-('e9dc9c56-e7ac-11ed-93d2-6045bdd1583d', CURRENT_DATE(),'Available', '{"CPU": "Intel Core i9-12900H Octo-core 20 threads", "GPU": "NVIDIA GeForce RTX 3070 8GB", "Memory": "DDR5 16GB 4800Hz Dual", "SSD": "SAMSUNG PM9A1 1TB", "Screen": "17.3-inch 4K OLED", "Ports": "4 x Thunderbolt 4"}', 'Laptop', 'Dell XPS 17 2022', 1, 14, 'A7', 2022, 1800.00);
+('e9dc9c56-e7ac-11ed-93d2-6045bdd1583d', CURRENT_DATE(),'Available', '{"CPU": "Intel Core i9-12900H Octo-core 20 threads", "GPU": "NVIDIA GeForce RTX 3070 8GB", "Memory": "DDR5 16GB 4800Hz Dual", "SSD": "SAMSUNG PM9A1 1TB", "Screen": "17.3-inch 4K OLED", "Ports": "4 x Thunderbolt 4"}', 'Laptop', 'Dell XPS 17 2022', 1, 14, 'A7', 2022, 1800.00),
+('b4ac3b32-af1c-11ec-94b7-6045bdd1583d', CURRENT_DATE(), 'Loaned', '{"CPU": "Intel Core i7-1185G7", "Memory": "16GB LPDDR4x", "SSD": "1TB", "Screen": "14-inch FHD", "Ports": "2 x Thunderbolt 4"}', 'Laptop', 'Lenovo ThinkPad X1 Carbon Gen 9', 1, 14, 'A12', 2021, 1600.00),
+('cde71f44-af1c-11ec-94b7-6045bdd1583d', CURRENT_DATE(), 'Loaned', '{"CPU": "M1 Pro 10-Core", "Memory": "16GB Unified", "SSD": "512GB", "Screen": "14-inch Retina Display", "Ports": "3 x Thunderbolt 4"}', 'Laptop', 'Apple MacBook Pro 2022 14-inch', 1, 14, 'A13', 2022, 1500.00),
+('e6bf12a6-af1c-11ec-94b7-6045bdd1583d', CURRENT_DATE(), 'Loaned', '{"CPU": "Intel Core i9-12900H Octo-core 20 threads", "GPU": "NVIDIA GeForce RTX 3080 10GB", "Memory": "DDR5 32GB 4800Hz Dual", "SSD": "SAMSUNG PM9A1 1TB", "Screen": "15.6-inch 4K OLED", "Ports": "4 x Thunderbolt 4"}', 'Laptop', 'Dell XPS 15 2022', 1, 14, 'A14', 2022, 2000.00);
 
--- --------------------------------------------------------
-
---
--- 表的结构 `loan`
---
-
+-- loan table
+-- note that startDate = start of reservation
+-- exten refers to how many times a loan has been extended
 DROP TABLE IF EXISTS `loan`;
 CREATE TABLE IF NOT EXISTS `loan` (
   `loanId` int(11) NOT NULL AUTO_INCREMENT,
@@ -91,17 +75,20 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- note that startDate = start of reservation
--- exten refers to how many times a loan has been extended
+
 INSERT INTO `loan` (`loanId`, `userId`, `userEmail`, `startDate`, `dueDate`, `deviceId`, `exten`, `returnedDate`)
 VALUES
 (1,'cdaix20', 'ucabcda@ucl.ac.uk', '2023-01-01', '2023-01-16', 'dff5d4ba-e7ac-11ed-93d2-6045bdd1583d', 0 , '2023-01-18'),
 (2,'cdaix20', 'ucabcda@ucl.ac.uk', '2023-02-01', '2023-03-16', 'e9dc9c56-e7ac-11ed-93d2-6045bdd1583d', 0 , '2023-02-18'),
-(3,'cdaix20', 'ucabcda@ucl.ac.uk', '2023-03-01', '2023-05-16', 'fb73ee46-e7ac-11ed-93d2-6045bdd1583d', 0 , 'null'),
-(4,'cdaix20', 'ucabcda@ucl.ac.uk', '2023-04-01', '2023-05-06', 'dab4de3d-e7ac-11ed-93d2-6045bdd1583d', 0 , 'null'),
-(5,'cdaix20', 'ucabcda@ucl.ac.uk', '2023-05-10', '2023-05-24', 'efbe9ea8-e7ab-11ed-93d2-6045bdd1583d', 0 , 'null'),
-(6,'jhudx92', 'ucabj38@ucl.ac.uk', '2023-04-01', '2023-04-24', 'bacf5a3c-e7ac-11ed-93d2-6045bdd1583d', 0 , '2023-04-24');
--- CALLED AT THE END OF EACH WEEK TO CLEAR RESERVATIONS
+(3,'cdaix20', 'ucabcda@ucl.ac.uk', '2023-03-01', '2023-05-16', 'fb73ee46-e7ac-11ed-93d2-6045bdd1583d', 0 , NULL),
+(4,'cdaix20', 'ucabcda@ucl.ac.uk', '2023-04-01', '2023-05-06', 'dab4de3d-e7ac-11ed-93d2-6045bdd1583d', 0 , NULL),
+(5,'cdaix20', 'ucabcda@ucl.ac.uk', '2023-05-10', '2023-05-24', 'efbe9ea8-e7ab-11ed-93d2-6045bdd1583d', 0 , NULL),
+(6,'jhudx92', 'ucabj38@ucl.ac.uk', '2023-04-01', '2023-04-24', 'bacf5a3c-e7ac-11ed-93d2-6045bdd1583d', 0 , '2023-04-24'),
+(7, 'tchaa15', 'ucabtc5@ucl.ac.uk', '2023-05-01', '2023-05-06', '051d4e1a-e7ac-11ed-93d2-6045bdd1583d', 0, NULL),
+(8, 'tchaa15', 'ucabtc5@ucl.ac.uk', '2023-05-02', '2023-05-09', '09a5423b-e7ac-11ed-93d2-6045bdd1583d', 0, NULL),
+(9, 'tchaa15', 'ucabtc5@ucl.ac.uk', '2023-05-03', '2023-05-10', 'a411ce8b-e7ac-11ed-93d2-6045bdd1583d', 0, NULL);
+
+-- EVENT CALLED AT THE END OF EACH WEEK TO CLEAR RESERVATIONS
 DROP EVENT IF EXISTS `clear_reservations`;
 DELIMITER $$
 CREATE EVENT IF NOT EXISTS `clear_reservations`
